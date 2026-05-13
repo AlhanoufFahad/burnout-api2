f573c3af48931f41782db8808cf8380f4d5b0106
Alhanouf Al-sharafi <Alhanooff200@gmail.com>
2026-05-13 20:19:48 +0300

    Update api.py

--- api.py
+++ api.py
@@ -22,7 +22,7 @@ app.add_middleware(
 
 class UserInput(BaseModel):
     designation: int = Field(ge=0, le=5)
-    resource_allocation: int = Field(ge=1, le=100)
+    resource_allocation: int = Field(ge=0, le=100)
     mental_fatigue_score: int = Field(ge=4, le=20)
 
 class BurnoutResponse(BaseModel):
