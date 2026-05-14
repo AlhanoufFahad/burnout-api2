5451b075a511ce179bdc1228122e5e34863eba8b
Alhanouf Al-sharafi <Alhanooff200@gmail.com>
2026-05-14 22:09:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,33 +4,21 @@ import numpy as np
 
 app = FastAPI()
 
-# تحميل الملفات
 model = joblib.load("model.pkl")
-scaler = joblib.load("scaler.pkl")
-
 
 @app.get("/")
 def home():
-    return {"message": "API running successfully"}
-
+    return {"message": "API running"}
 
 @app.post("/predict")
 def predict(data: dict):
 
-    designation = float(data["designation"])
-    resource_allocation = float(data["resource_allocation"])
-    mental_fatigue = float(data["mental_fatigue"])
+    x = np.array([[
+        float(data["designation"]),
+        float(data["resource_allocation"]),
+        float(data["mental_fatigue"])
+    ]])
 
-    features = np.array([
-        [designation, resource_allocation, mental_fatigue]
-    ])
+    prediction = model.predict(x)
 
-    # scaling
-    features_scaled = scaler.transform(features)
-
-    # prediction
-    prediction = model.predict(features_scaled)
-
-    return {
-        "prediction": float(prediction[0])
-    }
+    return {"prediction": float(prediction[0])}
